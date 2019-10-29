ec25b8da4a5671f34f986d4e3ae6b78b7854eb4a
haleonas <altes@live.se>
2019-10-29 16:07:55 +0100

    added firebase connection cleaned from earlier code and made a new branch

--- app/src/main/java/com/jesper/shutapp/MainActivity.java
+++ app/src/main/java/com/jesper/shutapp/MainActivity.java
@@ -1,14 +1,28 @@
 package com.jesper.shutapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
 }
