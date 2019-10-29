220d7a837d294983b0a81c9660d3672d479b2063
haleonas <altes@live.se>
2019-10-29 15:59:47 +0100

    first commit
    Mainactivity is the login screen. no functionality. can just go from MainActivity to RegisterUser
    
    Made layout for RegisterUser.
    added progressbar for making the user understand that the app is currently working.
    
    added activity for adding user (RegisterUser)
    checks for a valid email and matching password and confirm password.
    if all is true user is created via firebase connection.
    using firebase own onsuccess function for confirming that a user is truly created.

--- app/src/main/java/com/jesper/shutapp/MainActivity.java
+++ app/src/main/java/com/jesper/shutapp/MainActivity.java
@@ -1,14 +1,81 @@
 package com.jesper.shutapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //public FragmentManager fragmentManager;
+    private ProgressBar progressBar;
+    private EditText mEmail;
+    private EditText mPassword;
+
+    private String TAG = "Jesper";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView registerTxt = findViewById(R.id.register_link);
+        progressBar = findViewById(R.id.progressBar);
+        mEmail = findViewById(R.id.email_edittxt);
+        mPassword = findViewById(R.id.password_edittxt);
+
+        registerTxt.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Intent intent = new Intent(MainActivity.this, RegisterUser.class);
+                startActivity(intent);
+                return false;
+            }
+        });
     }
+
+    private void showProgress()
+    {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+    private void hideProgress()
+    {
+        if(progressBar.getVisibility() == View.VISIBLE)
+        {
+            progressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public void submitOnClick(View view)
+    {
+        String email = mEmail.getText().toString();
+        String password = mPassword.getText().toString();
+
+        if(isValidEmail(email))
+        {
+
+        }
+        showProgress();
+        //koppla till firebase
+    }
+    private Boolean isValidEmail(String email)
+    {
+        for(int i = 0; i < email.length(); i++)
+        {
+            if(email.charAt(i) == '@' && (email.contains(".com") || email.contains(".se")) )
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
---
+++ app/src/main/java/com/jesper/shutapp/RegisterUser.java
@@ -0,0 +1,121 @@
+package com.jesper.shutapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterUser extends AppCompatActivity {
+
+    private EditText mEmail;
+    private EditText mPassword;
+    private EditText mConfirmedPassword;
+    private ProgressBar mProgressbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_user_layout);
+
+        mEmail = findViewById(R.id.email_register_edittxt);
+        mPassword = findViewById(R.id.password_register_edittxt);
+        mConfirmedPassword = findViewById(R.id.confirm_password_register_edittxt);
+        mProgressbar = findViewById(R.id.progressBar);
+
+    }
+
+    public void registerOnClick(View view)
+    {
+        String email = mEmail.getText().toString();
+        String password = mPassword.getText().toString();
+        String confPassword = mConfirmedPassword.getText().toString();
+
+        if(!isEmpty(email) && !isEmpty(password) && !isEmpty(confPassword))
+        {
+           if(isValidEmail(email))
+           {
+               if(isMatchingPass(password,confPassword))
+               {
+
+                   showProgress();
+                   registerUser(email,password);
+                   Toast.makeText(this, "Creating user", Toast.LENGTH_SHORT).show();
+
+
+               }
+               else
+               {
+                    Toast.makeText(this,"Password doesn't match",Toast.LENGTH_SHORT).show();
+               }
+           }
+           else
+           {
+               Toast.makeText(this,"Not a valid Email",Toast.LENGTH_SHORT).show();
+           }
+        }
+        else
+        {
+            Toast.makeText(this,"You have to fill all the fields",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private Boolean isValidEmail(String email)
+    {
+        for(int i = 0; i < email.length(); i++)
+        {
+            if(email.charAt(i) == '@' && (email.contains(".com") || email.contains(".se")) )
+            {
+                return true;
+            }
+        }
+        return false;
+    } //att fixa
+    private Boolean isEmpty(String text)
+    {
+        return (text.equals(""));
+    }
+    private Boolean isMatchingPass(String password, String confirmedPassword)
+    {
+        return password.equals(confirmedPassword);
+    }
+
+    private void showProgress()
+    {
+        mProgressbar.setVisibility(View.VISIBLE);
+    }
+    private void hideProgress()
+    {
+        if (mProgressbar.getVisibility() == View.VISIBLE)
+        {
+            mProgressbar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void registerUser(String email, String password)
+    {
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(RegisterUser.this,"User created",Toast.LENGTH_SHORT).show();
+                    FirebaseAuth.getInstance().signOut();
+                }
+                else
+                {
+                    Toast.makeText(RegisterUser.this, "Unable to register", Toast.LENGTH_SHORT).show();
+                }
+                hideProgress();
+            }
+        });
+    }
+}
