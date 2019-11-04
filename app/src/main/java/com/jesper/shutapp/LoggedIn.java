143ac8a8612d18c3612e49fe5d17be454cd263db
amandayammine <amanda_yammine95@hotmail.com>
2019-11-04 12:48:36 +0100

    testing git

--- app/src/main/java/com/jesper/shutapp/LoggedIn.java
+++ app/src/main/java/com/jesper/shutapp/LoggedIn.java
@@ -27,6 +27,7 @@ public class LoggedIn extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.logged_in_toolbar);
         toolbar.setTitle(R.string.homepage_txt);
         setSupportActionBar(toolbar);
+        //commit
     }
 
     @Override
