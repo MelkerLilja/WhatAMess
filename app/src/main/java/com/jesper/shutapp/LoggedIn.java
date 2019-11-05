ae2614de0b307adae2e6d9ba836a5f494f65f001
anton <antongottfredsson@gmail.com>
2019-11-05 13:28:05 +0100

    Created a ChatActivity with simple ui to send message. Added button in LoggedIn to call ChatActivity. Added a Listview to hold the chat messages.

--- app/src/main/java/com/jesper/shutapp/LoggedIn.java
+++ app/src/main/java/com/jesper/shutapp/LoggedIn.java
@@ -6,15 +6,20 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.zip.Inflater;
 
 public class LoggedIn extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,17 @@ public class LoggedIn extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.logged_in_toolbar);
         toolbar.setTitle(R.string.homepage_txt);
         setSupportActionBar(toolbar);
+
+        btn = findViewById(R.id.btn_chat);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { //Created button and onclick to call the ChatActivity class
+                Intent intent = new Intent(LoggedIn.this, ChatActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
     @Override
