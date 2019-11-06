debffeb68d6410c503ad96f4c0e57d159a6a356b
marcusklaesson <55803843+marcusklaesson@users.noreply.github.com>
2019-11-06 17:28:44 +0100

    Fixed delete account, now it deletes the user from authentication in firebase but not the userlist.. yet...

--- app/src/main/java/com/jesper/shutapp/MainSettings.java
+++ app/src/main/java/com/jesper/shutapp/MainSettings.java
@@ -1,5 +1,6 @@
 package com.jesper.shutapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
@@ -7,13 +8,20 @@ import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainSettings extends AppCompatActivity {
 
@@ -25,6 +33,7 @@ public class MainSettings extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.logged_in_toolbar);
         toolbar.setTitle(R.string.settings_txt);
         setSupportActionBar(toolbar);
+
 
     }
 
@@ -48,7 +57,9 @@ public class MainSettings extends AppCompatActivity {
     // Enter terms of service fragment/activity
     public void terms_of_service(View view) {
 
+
     }
+
 
     // Log out and return to MainActivity
     public void log_out(View view) {
@@ -60,6 +71,24 @@ public class MainSettings extends AppCompatActivity {
 
     // Delete account and return to MainActivity
     public void delete_account(View view) {
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        user.delete()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("HEJ", "User account deleted.");
+                            //FirebaseDatabase.getInstance().getReference().child(getString
+                            // (R.string.db_users)).child(FirebaseAuth.getInstance().
+                            // getCurrentUser().getUid()).removeValue();
+                        }
+
+                    }
+                });
+        log_out(view);
+        finish();
 
 
     }
