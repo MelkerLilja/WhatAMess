f9231685fc93225be6c949955b0cc4bc43f31281
haleonas <altes@live.se>
2019-12-02 13:15:08 +0100

    fixing small problems wiht app icon and some xml that didn't get transfered after the merge. (Melker)

--- app/src/main/java/com/jesper/shutapp/Sqlite/SqlMain.java
+++ app/src/main/java/com/jesper/shutapp/Sqlite/SqlMain.java
@@ -13,6 +13,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -45,7 +46,7 @@ public class SqlMain extends AppCompatActivity {
             }
         });
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        ImageView fab = findViewById(R.id.add_report);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
